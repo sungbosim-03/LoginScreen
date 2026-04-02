76dd0ad9d51c64f81a63169e5928f2c508f44a32
sungbosim-33 <sungbosim5@suwon.ac.kr>
2026-04-02 12:35:13 +0900

    초기 커밋.

---
+++ Form1.cs
@@ -0,0 +1,64 @@
+namespace LoginScreen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtID_Enter(object sender, EventArgs e)
+        {
+            if (txtID.Text == "아이디")
+            {
+                txtID.Text = "";
+                txtID.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtID_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtID.Text))
+            {
+                txtID.Text = "아이디";
+                txtID.ForeColor = Color.Silver;
+            }
+        }
+
+        string myID = "admin";
+        string myPW = "superman";
+
+
+        private void btnLogin_Click_1(object sender, EventArgs e)
+        {
+            string inputID = txtID.Text;
+            string inputPW = txtPW.Text;
+            if (inputID == myID && inputPW == myPW)
+            {
+                MessageBox.Show("로그인성공!");
+            }
+            else { MessageBox.Show("로그인실패~"); }
+
+        }
+
+        private void txtPW_Leave_1(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtPW.Text))
+            {
+                txtPW.UseSystemPasswordChar = false;
+                txtPW.Text = "패스워드";
+                txtPW.ForeColor = Color.Silver;
+            }
+        }
+
+        private void txtPW_Enter_1(object sender, EventArgs e)
+        {
+            if (txtPW.Text == "패스워드")
+            {
+                txtPW.Text = "";
+                txtPW.ForeColor = Color.Black;
+                txtPW.UseSystemPasswordChar = true;
+            }
+        }
+    }
+}
